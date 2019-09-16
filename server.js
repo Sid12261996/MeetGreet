c8153b669545c74e23141a15074f3969beb2e109
Tarun Kethwalia <tarunkethwalia18@gmail.com>
2019-09-16 23:21:15 +0530

    Important changes

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express'),
+  app = express(),
+  path = require('path'),
+  cors= require('cors');
+
+app.use(cors());
+
+
+app.use(express.static(path.join(__dirname+'/build')));
+console.log(path.join(__dirname+'/dist/NoraWebsite'));
+app.listen(process.env.PORT||5000,()=>console.log('Connected!'));
+
+app.get('/*',(req,res)=>{
+  console.log("route..!")
+
+
+  res.sendFile(path.join(__dirname,'/build/index.html'));
+});
