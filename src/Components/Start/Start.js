84bbb816b8044e1e687e1522335bbf79a1246b3d
Silly Zan <tarunkethwalia18@gmail.com>
2019-10-14 10:30:07 +0530

    Login access

--- src/Components/Start/Start.js
+++ src/Components/Start/Start.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
 export default class Start extends Component {
+    handleLogout = () => {
+        this.props.history.replace("/");
+    }
     render() {
         return (
             <div>
                 <h1>Inside</h1>
-                <button>Logout</button>
+                <button onClick={this.handleLogout}>Logout</button>
             </div>
         )
     }
