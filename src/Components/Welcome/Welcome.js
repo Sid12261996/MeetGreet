097cec0be44f814273d08e89a5f3b4f3232be93f
Tarun <tarunkethwalia18@gmail.com>
2019-11-23 00:46:22 +0530

    Image Dimensions Set

---
+++ src/Components/Welcome/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Welcome.css';
+
+function Welcome() {
+    return (
+        <div>
+            Loading....
+        </div>
+    )
+}
+
+export default Welcome;
