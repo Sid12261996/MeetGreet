21776c3ad53ba0734048742e8a86a4999468ae60
Tarun <tarunkethwalia18@gmail.com>
2020-03-15 23:52:32 +0530

    Profile Design completed

--- src/services/image-service.js
+++ src/services/image-service.js
@@ -1,12 +1,17 @@
 import axios from "axios";
-import env from "../environment"
+import env from "../environment";
 
 const URL = `${env.ImageBaseUrl}`;
+const ApiLink = `${env.url}`;
 
 class imageService {
 
     static upload = (formData,config) => {
         return axios.post(`${URL}`, formData,config);
+    };
+
+    static ProfilePic = (userId,PPUrl) => {
+        return axios.post(`${ApiLink}user/${userId}/${PPUrl}/userPicUpdate`);
     };
 }
 
