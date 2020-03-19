d8ea8a9e0731cc1303cd012ae839038ed2d810d0
Tarun <tarunkethwalia18@gmail.com>
2020-03-19 20:23:58 +0530

    Fixed Issues

--- src/services/user-services.js
+++ src/services/user-services.js
@@ -6,6 +6,12 @@ const userURL = `${env.ApiLink}user/`;
 
 class userService {
 
+    // On Development, Comment the MGLink
+    // Before pushing on Cloud, Comment the url2
+    // static LoadUrl = `${env.url2}`;
+    static LoadUrl = `${env.MGLink}`;
+
+
    static login = (user) => {
         return axios.post(`${userURL}login`, user);
     };
