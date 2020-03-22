4814868126160dac218da7e114a48fcbbaf21569
sid12261996 <sidharthrkc@gmail.com>
2020-03-23 01:09:44 +0530

    https issue

--- src/environment.js
+++ src/environment.js
@@ -2,7 +2,7 @@ const environment = {
     // ApiLink: "http://localhost:5000/api/",
     // MGLink: "http://localhost:3000",
     ApiLink: "https://meetgreet-api.herokuapp.com/api/",
-    MGLink: "http://meetgreet-beta.herokuapp.com",
+    MGLink: "https://meetgreet-beta.herokuapp.com",
     ImageBaseUrl: "https://meetgreet-upload.herokuapp.com/",
 
     // ApiLink: "https://meetgreet-api-nov.herokuapp.com/api/"
