4d89efec2d1b7d116afa6d38d6d812b0f5db8b82
NasirAli1419 <1419nasir@gmail.com>
2020-04-18 15:00:43 +0530

    Added dobView in registration bloack

--- controllers/userController.js
+++ controllers/userController.js
@@ -76,6 +76,7 @@ exports.Register = (req, res) => {
                                 Status: req.body.Status,
                                 Profession: req.body.Profession,
                                 DateOfBirth:req.body.DateOfBirth,
+                                DOBView:req.body.DOBView,
                                 City: req.body.City,
                                 Country: req.body.Country,
                                 // Security: req.body.Security,
