b19a9035408e9960e42bf23f8e58f906a51191a0
tarunkethwalia <tarunkethwalia18@gmail.com>
2020-05-15 17:03:18 +0530

    few changes in userModel

--- models/userModel.js
+++ models/userModel.js
@@ -38,9 +38,9 @@ const User = new schema({
     // Post: ["Status", "Images", "Shared-post", "Videos"],
     JobProfile: [String],
     ImageUrl: {type: String, default: 'default'},
-    CoverUrl: {type: schema.Types.String, default: 'default'},
-    Status:{type: schema.Types.String,default: `Lets's get started`},
-    Profession:{type:schema.Types.String,defalut: null}
+    CoverUrl: {type: String, default: 'default'},
+    Status:{type: schema.Types.String, default: `Lets's get started`},
+    Profession:{type:schema.Types.String, default: null}
     
 });
 
